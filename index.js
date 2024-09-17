adc16a5f1b05ac169beccc6350bd64c831767906
In44ne <ins44ne91@gmail.com>
2024-09-17 13:40:55 +0300

    feat: 10/10 daily completed

--- index.js
+++ index.js
@@ -199,3 +199,17 @@ console.log(reverseWords("foobar"), "foobar");
 console.log(reverseWords("kata editor"), "editor kata");
 console.log(reverseWords("row row row your boat"), "boat your row row row");
 console.log(reverseWords(""), "");
+
+/* #10
+Combine strings function
+Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+*/
+
+function combineNames(a, b) {
+  return `${a} ${b}`;
+}
+
+console.log(combineNames("James", "Stevens"), "James Stevens");
