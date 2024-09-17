ce9147700f36f3cea9202d8e52ce497fcb7fe428
In44ne <ins44ne91@gmail.com>
2024-09-17 11:07:51 +0300

    feat: 3/10 daily completed

--- index.js
+++ index.js
@@ -38,3 +38,25 @@ function isDivisible(n, x, y) {
   console.log(isDivisible(12,3,4),true);
   console.log(isDivisible(8,3,4),false);
   console.log(isDivisible(48,3,4),true);
+
+  /* #3
+  Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.
+
+*/
+
+function areYouPlayingBanjo(name) {
+    return (name[0].toLowerCase() === 'r') ? `${name} plays banjo` : `${name} does not play banjo` 
+    }
+
+  console.log(areYouPlayingBanjo("Adam"), "Adam does not play banjo");
+  console.log(areYouPlayingBanjo("Paul"), "Paul does not play banjo");
+  console.log(areYouPlayingBanjo("Ringo"), "Ringo plays banjo");
+  console.log(areYouPlayingBanjo("bravo"), "bravo does not play banjo");
+  console.log(areYouPlayingBanjo("rolf"), "rolf plays banjo");
