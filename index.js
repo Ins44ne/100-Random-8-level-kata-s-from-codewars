b1ba9a47ae6eef072225e92a4bcb900e59a487a8
In44ne <ins44ne91@gmail.com>
2024-09-18 12:35:41 +0300

    feat: 1/10 daily completed

--- index.js
+++ index.js
@@ -211,5 +211,25 @@ With "James" as the first name and "Stevens" as the last name should return "Jam
 function combineNames(a, b) {
   return `${a} ${b}`;
 }
-
 console.log(combineNames("James", "Stevens"), "James Stevens");
+
+/* #11
+Create a function with two arguments that will return an array of the first n multiples of x.
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+Return the results as an array or list ( depending on language ).
+
+Examples
+countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+countBy(2,5) === [2,4,6,8,10]
+*/
+
+function countBy(x, n) {
+  let res = [];
+  for (i = 1; i <= n; i += 1) {
+    res.push(x * i);
+  }
+  return res;
+}
+
+console.log(countBy(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(countBy(2, 5), [2, 4, 6, 8, 10]);
