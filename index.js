6c7011b2701c5c70053a6a41ecc0ab55abeb631b
In44ne <ins44ne91@gmail.com>
2024-09-18 14:53:48 +0300

    feat: 7/10 daily completed

--- index.js
+++ index.js
@@ -344,8 +344,19 @@ function removeExclamationMarks(s) {
 console.log(removeExclamationMarks("Hello World!"), "Hello World");
 
 /* #17
-
+Build a function that returns an array of integers from n to 1 where n>0.
+Example : n=5 --> [5,4,3,2,1]
 */
+
+const reverseSeq = (n) => {
+  let res = [];
+  for (let i = n; i > 0; i -= 1) {
+    res.push(i);
+  }
+  return res;
+};
+
+console.log(reverseSeq(5), [5, 4, 3, 2, 1]);
 
 /* #18
 
