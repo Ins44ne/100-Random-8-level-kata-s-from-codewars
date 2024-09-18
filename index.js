72e201a992f118453fb780cbcc10607c508a5eed
In44ne <ins44ne91@gmail.com>
2024-09-18 15:13:35 +0300

    feat: 8/10 daily completed

--- index.js
+++ index.js
@@ -359,9 +359,36 @@ const reverseSeq = (n) => {
 console.log(reverseSeq(5), [5, 4, 3, 2, 1]);
 
 /* #18
-
+Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+Mind the input validation.
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
 */
 
+function sumArray(array) {
+  let res = 0;
+  if (!(typeof array === "object") || array === null || array.length < 2) {
+    return 0;
+  }
+  array.sort(function (a, b) {
+    return a - b;
+  });
+  for (let i = 1; i < array.length - 1; i += 1) {
+    res += array[i];
+  }
+  return res;
+}
+
+console.log(sumArray(null), 0);
+console.log(sumArray([]), 0);
+console.log(sumArray([3]), 0);
+console.log(sumArray([3, 5]), 0);
+console.log(sumArray([6, 2, 1, 8, 10]), 16);
+console.log(sumArray([0, 1, 6, 10, 10]), 17);
+console.log(sumArray([-6, -20, -1, -10, -12]), -28);
+console.log(sumArray([-6, 20, -1, 10, -12]), 3);
 /* #19
 
 */
