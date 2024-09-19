674a468d8ca8aff8fd6934c240e4a7ffe537aec1
In44ne <ins44ne91@gmail.com>
2024-09-19 11:01:47 +0300

    feat: 6/10 daily completed

--- index.js
+++ index.js
@@ -517,9 +517,21 @@ function mango(quantity, price) {
 
 console.log(mango(3, 3), 6);
 console.log(mango(9, 5), 30);
-/* #26
- */
 
+/* #26
+Your task is to find the nearest square number of a positive integer n. In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+For example, if n = 111, then the nearest square number equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
+If n is already a perfect square (e.g. n = 144, n = 81, etc.), you need to just return n. */
+
+function nearestSq(n) {
+  return Math.pow(Math.round(Math.sqrt(n)), 2);
+}
+
+console.log(nearestSq(1), 1, "nearestSq(1) = 1");
+console.log(nearestSq(2), 1, "nearestSq(2) = 1");
+console.log(nearestSq(10), 9, "nearestSq(10) = 9");
+console.log(nearestSq(111), 121, "nearestSq(111) = 121");
+console.log(nearestSq(9999), 10000, "nearestSq(9999) = 10000");
 /* #27
  */
 
