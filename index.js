f164a0f0c37145308163c9f1eadedccc7aee2455
In44ne <ins44ne91@gmail.com>
2024-09-19 10:03:23 +0300

    feat: 1/10 daily completed

--- index.js
+++ index.js
@@ -428,3 +428,14 @@ console.log(arrayPlusArray([1, 2, 3], [4, 5, 6]), 21);
 console.log(arrayPlusArray([-1, -2, -3], [-4, -5, -6]), -21);
 console.log(arrayPlusArray([0, 0, 0], [4, 5, 6]), 15);
 console.log(arrayPlusArray([100, 200, 300], [400, 500, 600]), 2100);
+
+/* #21
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+function boolToWord(bool) {
+  return bool === true ? "Yes" : "No";
+}
+
+console.log(boolToWord(true), "Yes");
+console.log(boolToWord(false), "No");
