91247fe610b16322b0db6b4c141abe8c3e8b099f
In44ne <ins44ne91@gmail.com>
2024-09-19 11:06:18 +0300

    feat: 7/10 daily completed

--- index.js
+++ index.js
@@ -533,7 +533,20 @@ console.log(nearestSq(10), 9, "nearestSq(10) = 9");
 console.log(nearestSq(111), 121, "nearestSq(111) = 121");
 console.log(nearestSq(9999), 10000, "nearestSq(9999) = 10000");
 /* #27
+Terminal game move function
+In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+Example:
+move(3, 6) should equal 15
  */
+
+function move(pos, roll) {
+  return pos + roll * 2;
+}
+
+console.log(move(0, 4), 8);
+console.log(move(3, 6), 15);
+console.log(move(2, 5), 12);
 
 /* #28
  */
