326ee12fa82b56392a19f3b12680b7cf835d94a8
In44ne <ins44ne91@gmail.com>
2024-09-19 10:16:06 +0300

    feat: 3/10 daily completed

--- index.js
+++ index.js
@@ -467,8 +467,26 @@ console.log(max([4, 6, 2, 1, 9, 63, -134, 566]), 566);
 console.log(max([5]), 5);
 
 /* #23
+Complete the function which takes two arguments and returns all numbers which are divisible by the given divisor. First argument is an array of numbers and the second is the divisor.
+Example(Input1, Input2 --> Output)
+[1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
  */
 
+function divisibleBy(numbers, divisor) {
+  let res = [];
+  numbers.forEach((el) => {
+    if (el % divisor === 0) {
+      res.push(el);
+    }
+  });
+  return res;
+}
+
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 2), [2, 4, 6]);
+console.log(divisibleBy([1, 2, 3, 4, 5, 6], 3), [3, 6]);
+console.log(divisibleBy([0, 1, 2, 3, 4, 5, 6], 4), [0, 4]);
+console.log(divisibleBy([0], 4), [0]);
+console.log(divisibleBy([1, 3, 5], 2), []);
 /* #24
  */
 
