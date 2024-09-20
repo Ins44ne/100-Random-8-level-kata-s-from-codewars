8422691f3e3ef7a1b2ace2abf9133d66684a931e
In44ne <ins44ne91@gmail.com>
2024-09-20 20:33:38 +0300

    feat: 9/10 daily completed

--- index.js
+++ index.js
@@ -807,9 +807,20 @@ console.log(distinct([1, 2]), [1, 2]);
 console.log(distinct([1, 1, 2]), [1, 2]);
 
 /* #39
+Create a combat function that takes the player's current health and the amount of damage received, and returns the player's new health. 
+Health can't be less than 0.
  */
 
+function combat(h, d) {
+  return h - d > 0 ? h - d : 0;
+}
+
+console.log(combat(100, 5), 95);
+console.log(combat(92, 8), 84);
+console.log(combat(20, 30), 0, "Health cannot go below 0");
+
 /* #40
+
  */
 
 /* #41
