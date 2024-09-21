a0d4e7448023b3e290ed3cdfd5fe2b77d65ae2d1
In44ne <ins44ne91@gmail.com>
2024-09-21 10:31:51 +0300

    feat: 10/10 daily completed

--- index.js
+++ index.js
@@ -1153,7 +1153,37 @@ function makeNegative(num) {
 console.log(makeNegative(42), -42);
 
 /* #50
+Complete the function which returns the weekday according to the input number:
+1 returns "Sunday"
+2 returns "Monday"
+3 returns "Tuesday"
+4 returns "Wednesday"
+5 returns "Thursday"
+6 returns "Friday"
+7 returns "Saturday"
+Otherwise returns "Wrong, please enter a number between 1 and 7"
  */
+
+function whatday(num) {
+  const arr = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return num > arr.length || num === 0
+    ? "Wrong, please enter a number between 1 and 7"
+    : arr[num - 1];
+}
+
+console.log(whatday(1), "Sunday");
+console.log(whatday(2), "Monday");
+console.log(whatday(3), "Tuesday");
+console.log(whatday(8), "Wrong, please enter a number between 1 and 7");
+console.log(whatday(20), "Wrong, please enter a number between 1 and 7");
 
 /* #51
  */
