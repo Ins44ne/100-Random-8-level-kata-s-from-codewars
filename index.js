83ccc99a0a80b5968aec9ba6ea90271ec3b66984
In44ne <ins44ne91@gmail.com>
2024-09-21 10:18:53 +0300

    feat: 8/10 daily completed

--- index.js
+++ index.js
@@ -1108,7 +1108,31 @@ console.log(
 );
 
 /* #48
+Task
+Provided is a function Kata which accepts two parameters in the following order: array, element and returns the index of the element if found and "Not found" otherwise.
+Your task is to shorten the code as much as possible in order to meet the strict character count requirements of the Kata. 
+(no more than 161) You may assume that all array elements are unique.
  */
+
+const find = (arr, elem) =>
+  arr.indexOf(elem) !== -1 ? arr.indexOf(elem) : "Not found";
+
+console.log(find(array, 5), 2);
+console.log(find(array, 11), 4);
+console.log(find(array, 3), 1);
+console.log(find(array, 2), 0);
+console.log(find(array, 7), 3);
+console.log(find(array, 1), "Not found");
+console.log(find(array, 8), "Not found");
+array = [true, "Hello World", false, "Lorem Ipsum", 6, Math.PI];
+console.log(find(array, "Hello World"), 1);
+console.log(find(array, "lorem ipsum"), "Not found");
+console.log(find(array, "Lorem Ipsum"), 3);
+console.log(find(array, false), 2);
+console.log(find(array, true), 0);
+console.log(find(array, Math.PI), 5);
+console.log(find(array, 3.14), "Not found");
+console.log(find(array, 6), 4);
 
 /* #49
  */
