92740028e34172eb4e9be73148c7a7ea58c1e612
In44ne <ins44ne91@gmail.com>
2024-09-21 10:15:06 +0300

    feat: 7/10 daily completed

--- index.js
+++ index.js
@@ -1062,7 +1062,50 @@ console.log(humanYearsCatYearsDogYears(2), [2, 24, 24]);
 console.log(humanYearsCatYearsDogYears(10), [10, 56, 64]);
 
 /* #47
+In this first kata in the series, you need to define a Hero prototype to be used in a terminal game. The hero should have the following attributes:
+attribute	value
+name	user argument or 'Hero'
+position	'00'
+health	100
+damage	5
+experience	0
  */
+
+function Hero(name = "Hero") {
+  this.name = name;
+  this.position = "00";
+  this.health = 100;
+  this.damage = 5;
+  this.experience = 0;
+}
+
+let myHero = new Hero();
+
+console.log(
+  myHero.name,
+  "Hero",
+  "Hero should have a 'name' attribute with value \"Hero\""
+);
+console.log(
+  myHero.experience,
+  0,
+  "Hero should have an 'experience' attribute with value 0"
+);
+console.log(
+  myHero.health,
+  100,
+  "Hero should have a 'health' attribute with value 100"
+);
+console.log(
+  myHero.position,
+  "00",
+  "Hero should have a 'position' attribute with value \"00\""
+);
+console.log(
+  myHero.damage,
+  5,
+  "Hero should have a 'damage' attribute with value 5"
+);
 
 /* #48
  */
