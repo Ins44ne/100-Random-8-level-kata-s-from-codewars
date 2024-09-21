9bce05c9f0e76617567623665487e06ea0363f28
In44ne <ins44ne91@gmail.com>
2024-09-21 09:44:46 +0300

    feat: 2/10 daily completed

--- index.js
+++ index.js
@@ -896,3 +896,67 @@ console.log(getGrade(58, 62, 70), "D");
 console.log(getGrade(44, 55, 52), "F");
 console.log(getGrade(48, 55, 52), "F");
 console.log(getGrade(58, 59, 60), "F");
+
+/* #42
+Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+"hello world".toAlternatingCase() === "HELLO WORLD"
+"HELLO WORLD".toAlternatingCase() === "hello world"
+"hello WORLD".toAlternatingCase() === "HELLO world"
+"HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+"12345".toAlternatingCase()       === "12345"                   // Non-alphabetical characters are unaffected
+"1a2b3c4d5e".toAlternatingCase()  === "1A2B3C4D5E"
+"String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+As usual, your function/method should be pure, i.e. it should not mutate the original string.
+*/
+
+String.prototype.toAlternatingCase = function () {
+  return this.split("")
+    .map((char) => {
+      return char === char.toUpperCase()
+        ? char.toLowerCase()
+        : char.toUpperCase();
+    })
+    .join("");
+};
+
+console.log("hello world".toAlternatingCase(), "HELLO WORLD");
+console.log("HELLO WORLD".toAlternatingCase(), "hello world");
+console.log("hello WORLD".toAlternatingCase(), "HELLO world");
+console.log("HeLLo WoRLD".toAlternatingCase(), "hEllO wOrld");
+console.log("12345".toAlternatingCase(), "12345");
+console.log("1a2b3c4d5e".toAlternatingCase(), "1A2B3C4D5E");
+console.log(
+  "String.prototype.toAlternatingCase".toAlternatingCase(),
+  "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+);
+console.log(
+  "Hello World".toAlternatingCase().toAlternatingCase(),
+  "Hello World"
+);
+
+/* #43
+ */
+
+/* #44
+ */
+
+/* #45
+ */
+
+/* #46
+ */
+
+/* #47
+ */
+
+/* #48
+ */
+
+/* #49
+ */
+
+/* #50
+ */
+
+/* #51
+ */
