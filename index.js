39d57f9d9658b23e315f6f8b8590c96ba1b86f2a
In44ne <ins44ne91@gmail.com>
2024-09-21 09:57:45 +0300

    feat: 5/10 daily completed

--- index.js
+++ index.js
@@ -1005,7 +1005,21 @@ console.log(
 );
 
 /* #45
+Create a function called _if which takes 3 arguments: a value bool and 2 functions (which do not take any parameters): func1 and func2
+When bool is truthy, func1 should be called, otherwise call the func2.
+Example:
+_if(true, function(){console.log("True")}, function(){console.log("false")})
+// Logs 'True' to the console.
  */
+
+function _if(bool, func1, func2) {
+  console.log(bool);
+  if (bool == true) {
+    func1();
+  } else {
+    func2();
+  }
+}
 
 /* #46
  */
