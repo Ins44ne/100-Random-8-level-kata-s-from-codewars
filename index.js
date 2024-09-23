0d2c80550b9e723121ee962db98f136756b7592f
In44ne <ins44ne91@gmail.com>
2024-09-23 20:46:26 +0300

    feat: 10/10 daily completed

--- index.js
+++ index.js
@@ -1417,6 +1417,25 @@ function isToday(date) {
 }
 
 /* #60
+Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives two parameters.
+The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm).
+It should return false otherwise. Examples:
+employed | vacation 
+true     | true     => false
+true     | false    => true
+false    | true     => false
+false    | false    => false
+FundamentalsLogic
  */
+
+function setAlarm(employed, vacation){
+  return employed === true && vacation === false
+ }
+
+ console.log(setAlarm(true, true),  false,"Should be false.");
+ console.log(setAlarm(false,true),  false, "Should be false.");
+ console.log(setAlarm(true, false), true,"Should be true.");
+
 /* #61
  */
