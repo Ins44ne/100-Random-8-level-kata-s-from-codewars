374a24cbc45bacf93ae38523f44aadf66af83b7c
In44ne <ins44ne91@gmail.com>
2024-09-23 20:19:31 +0300

    feat: 6/10 daily completed

--- index.js
+++ index.js
@@ -1357,7 +1357,18 @@ console.log(problem(""), "Error");
 console.log(problem(0.03), 7.5);
 
 /* #56
- */
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24 
+*/
+
+function grow(x){
+  return x.reduce((a, b)=> a * b,1);
+}
+
+console.log(grow([1, 2, 3]), 6);
+console.log(grow([4, 1, 1, 1, 4]), 16); 
+console.log(grow([2, 2, 2, 2, 2, 2]), 64); 
+
 /* #57
  */
 /* #58
