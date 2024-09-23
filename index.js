0505d84d2754c73df7d39d4ead76d577fef1377b
In44ne <ins44ne91@gmail.com>
2024-09-23 20:15:32 +0300

    feat: 5/10 daily completed

--- index.js
+++ index.js
@@ -1337,9 +1337,25 @@ console.log((amIWilson(5), true))
 console.log((amIWilson(9), false))
 console.log((amIWilson(6), false))
 
-
 /* #55
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
  */
+
+function problem(x){
+  return typeof x === 'string' ? "Error" : x*50+6
+
+}
+console.log(problem("hello"), "Error");
+console.log(problem(1), 56);
+console.log(problem(5), 256);
+console.log(problem(0), 6);
+console.log(problem(1.2), 66);
+console.log(problem(3), 156);
+console.log(problem("RyanIsCool"), "Error");
+console.log(problem(-3), -144);
+console.log(problem(""), "Error");
+console.log(problem(0.03), 7.5);
+
 /* #56
  */
 /* #57
