c91c903f93a083278d746f84fb6705079141b7b7
In44ne <ins44ne91@gmail.com>
2024-09-23 20:24:51 +0300

    feat: 7/10 daily completed

--- index.js
+++ index.js
@@ -1370,7 +1370,27 @@ console.log(grow([4, 1, 1, 1, 4]), 16);
 console.log(grow([2, 2, 2, 2, 2, 2]), 64); 
 
 /* #57
+Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside.
+The strings will not be the same length, but they may be empty ( zero length ).
+Hint for R users:
+The length of string is not always the same as the number of characters
+For example: (Input1, Input2) --> output
+("1", "22") --> "1221"
+("22", "1") --> "1221"
+ShortLongShort.solution("1", "22"); // returns "1221"
+ShortLongShort.solution("22", "1"); // returns "1221"
  */
+
+function solution(a, b) {
+  return a.length < b.length ? a + b + a : b + a + b
+}
+  
+  console.log(solution('45', '1'), '1451');
+  console.log(solution('13', '200'), '1320013');
+  console.log(solution('Soon', 'Me'), 'MeSoonMe');
+  console.log(solution('U', 'False'), 'UFalseU');
+
+
 /* #58
  */
 /* #59
