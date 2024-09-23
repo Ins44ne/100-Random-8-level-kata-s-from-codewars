39586edde5af3448ee67b8d8f70eb618ac18de9f
In44ne <ins44ne91@gmail.com>
2024-09-23 19:42:56 +0300

    feat: 1/10 daily completed

--- index.js
+++ index.js
@@ -1186,4 +1186,57 @@ console.log(whatday(8), "Wrong, please enter a number between 1 and 7");
 console.log(whatday(20), "Wrong, please enter a number between 1 and 7");
 
 /* #51
+Given three integers a, b, and c, return the largest number obtained after inserting the operators +, *, and parentheses (). In other words, try every combination of a, b, and c with the operators, without reordering the operands, and return the maximum value.
+Example
+With the numbers 1, 2, and 3, here are some possible expressions:
+1 * (2 + 3) = 5
+1 * 2 * 3 = 6
+1 + 2 * 3 = 7
+(1 + 2) * 3 = 9
+The maximum value that can be obtained is 9.
+Notes
+The numbers are always positive, in the range 1 ≤ a, b, c ≤ 10.
+You can use the same operation more than once.
+It is not necessary to use all the operators or parentheses.
+You cannot swap the operands. For example, with the given numbers, you cannot get the expression (1 + 3) * 2 = 8.
+Input and Output Examples
+expressionsMatter(1, 2, 3) ==> 9, because (1 + 2) * 3 = 9.
+expressionsMatter(1, 1, 1) ==> 3, because 1 + 1 + 1 = 3.
+expressionsMatter(9, 1, 1) ==> 18, because 9 * (1 + 1) = 18.
  */
+
+function expressionMatter(a, b, c) {
+  return Math.max(
+    a + b + c,
+    a * b * c,
+    a * (b + c),
+    (a + b) * c,
+    a + b * c,
+    a * b + c
+  );
+}
+
+// Small values
+assert.strictEqual(expressionMatter(2, 1, 2), 6);
+assert.strictEqual(expressionMatter(2, 1, 1), 4);
+assert.strictEqual(expressionMatter(1, 1, 1), 3);
+assert.strictEqual(expressionMatter(1, 2, 3), 9);
+assert.strictEqual(expressionMatter(1, 3, 1), 5);
+assert.strictEqual(expressionMatter(2, 2, 2), 8);
+
+// Medium values
+assert.strictEqual(expressionMatter(5, 1, 3), 20);
+assert.strictEqual(expressionMatter(3, 5, 7), 105);
+assert.strictEqual(expressionMatter(5, 6, 1), 35);
+assert.strictEqual(expressionMatter(1, 6, 1), 8);
+assert.strictEqual(expressionMatter(2, 6, 1), 14);
+assert.strictEqual(expressionMatter(6, 7, 1), 48);
+
+// Mixed values
+assert.strictEqual(expressionMatter(2, 10, 3), 60);
+assert.strictEqual(expressionMatter(1, 8, 3), 27);
+assert.strictEqual(expressionMatter(9, 7, 2), 126);
+assert.strictEqual(expressionMatter(1, 1, 10), 20);
+assert.strictEqual(expressionMatter(9, 1, 1), 18);
+assert.strictEqual(expressionMatter(10, 5, 6), 300);
+assert.strictEqual(expressionMatter(1, 10, 1), 12);
