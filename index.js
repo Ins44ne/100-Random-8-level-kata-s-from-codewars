821265461f6f4d4b6899045e7e50e4eb9eaf9735
In44ne <ins44ne91@gmail.com>
2024-09-23 20:43:23 +0300

    feat: 9/10 daily completed

--- index.js
+++ index.js
@@ -1405,7 +1405,17 @@ console.log(hoopCount(3),"Keep at it until you get it" )
 console.log(hoopCount(11),"Great, now move on to tricks" )
 
 /* #59
+Write a simple function that takes as a parameter a date object and returns a boolean value representing whether the date is today or not.
+Make sure that your function does not return a false positive by only checking the day.
  */
+
+function isToday(date) {
+  const today = new Date();  
+  return date.getDate() === today.getDate() &&
+         date.getMonth() === today.getMonth() &&
+         date.getFullYear() === today.getFullYear();
+}
+
 /* #60
  */
 /* #61
